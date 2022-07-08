03c7748eede2bf3a2b3b9df7646ed709121b0322
Rushikesh Jadhav <81899664+rushikeshjadhav1352@users.noreply.github.com>
2022-07-09 01:03:23 +0530

    Time: 51 ms (28.67%), Space: 93.8 MB (45.72%) - LeetHub

--- 219-contains-duplicate-ii/219-contains-duplicate-ii.java
+++ 219-contains-duplicate-ii/219-contains-duplicate-ii.java
@@ -3,8 +3,8 @@ class Solution {
         HashMap<Integer,Integer> ans=new HashMap<Integer,Integer>();
        for(int i=0;i<nums.length;i++){
            if(ans.containsKey(nums[i])){
-               int sum=ans.get(nums[i]);
-               if(Math.abs(sum-i)<=k){
+               //int sum=ans.get(nums[i]);
+               if(Math.abs(ans.get(nums[i])-i)<=k){
                        return true;
                    }
            }
